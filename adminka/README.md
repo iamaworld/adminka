f70ac67220b4f542f38cdd6196c464617f66045a
iamheaven <125787194+iamaworld@users.noreply.github.com>
2024-05-04 12:34:34 +0500

    Add files via upload

---
+++ adminka/README.md
@@ -0,0 +1,3 @@
+# adminka
+# adminka
+# adminka
---
+++ adminka/routes/games.js
@@ -0,0 +1,16 @@
+// routes/games.js
+const gamesRouter = require("express").Router(); // Создали роутер
+const {
+  sendAllGames,
+  deleteGame,
+  addGameController,
+} = require("../controllers/games");
+const { getAllGames } = require("../middlewares/games");
+
+// Получим игры из JSON-файла и отправим в ответ на запрос
+
+gamesRouter.post("/games", getAllGames, addGameController);
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+
+module.exports = gamesRouter;
